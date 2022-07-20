adccbd618757dd5e864e35ef67b6a2fc263a3485
Jacek Janczura <jacek.janczura@jadenx.com>
2022-07-20 21:35:30 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# client-server
+An example implementation of super simple oAuth2 client - spring boot - which uses custom external oauth2 provider
+
+It's a project I created to show how to use an external oAuth2 provider
+
+Build it with maven `mvn clean verify` run `java -jar <jarname>`
---
+++ src/main/java/com/janczura/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.janczura.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        return http.build();
+    }
+}
